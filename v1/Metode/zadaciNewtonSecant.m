347966ac453e30a0a023ac99dbc9237e64b4a458
vlaksi <daksrapsody@gmail.com>
2019-11-21 10:57:41 +0100

    v1 zad sa secicom

--- v1/Metode/zadaciNewtonSecant.m
+++ v1/Metode/zadaciNewtonSecant.m
@@ -17,4 +17,22 @@ function y = zadaciNewtonSecant()
     a = 1;
     tol = 10.^-2;
     [x, fx, n] = newton(fun, dfun, d2fun, a, tol)
+    
+    %% Metoda secice - Primer sa vezbi
+    fun = @(x) 2*x.^4 - 3*x;
+    dfun = @(x) 8*x.^3 -3;
+    
+    [x,fx,n] = secica(fun,dfun,1,2,10.^-2)
+    
+    %% Zadatak 2 - Metodom se?ice odrediti minimum
+    % f(x) = 4x^3 - 15x^2 - 4x + 24
+    %a = 3
+    %b = 4
+    %? = 0.001
+    
+    fun=@(x) 4*x.^3 - 15*x.^2 - 4*x +24;
+    dfun=@(x) 12*x.^2 - 30*x - 4;
+    
+    [x,fx,n] = secica(fun,dfun,3,4,0.001)
+
 end
