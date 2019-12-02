59619ca7c8ad3d808dfbb19ee3ae81577b973bd0
vlaksi <daksrapsody@gmail.com>
2019-12-02 15:49:40 +0100

    Dodan test i kod za gradijentni metod

--- v2/metode/gradijent2/grad_descent.m
+++ v2/metode/gradijent2/grad_descent.m
@@ -1,40 +1,18 @@
-function [xopt,fopt,niter,gnorm,dx] = grad_descent(f,grad,gamma, tol,x0)
-gnorm = inf;
-x = x0; 
-niter = 0; 
-dx = inf;
-
-%%
-
-xarray=-3:0.1:3; 
-[X1, X2] = meshgrid(xarray, xarray);
-Y = NaN*ones(size(X1));
-for i = 1:size(X1,1)
-    for j = 1:size(X1,2)
-        Y(i, j) = f([X1(i,j); X2(i,j)]);
+function [xopt,fopt,it,gnorm] = grad_descent(f,grad,gamma, tol,x0)
+    gnorm = inf;                % duzina vektora tj || gradijent || na pocetku inf da bi cisto usli u while
+    x = x0; 
+    it = 0; 
+  
+    while gnorm>=tol            % kriterijum zaustavljanja || gradijent || < tol
+        g = grad(x);            % gradijent 
+        gnorm = norm(g);        % || gradijent || = norm(g) da bi dobili duzinu vektora
+        
+        xnew = x - gamma*g;     % novo x je po formuli,- jer je SteepestDescent 
+        it = it + 1;            % sledeca iteracija 
+        x = xnew;
     end
-end
-figure
-surface(X1,X2,Y)
-grid on
-%%
-
-
-while gnorm>=tol
-    g = grad(x);
-    gnorm = norm(g);
-    xnew = x - gamma*g;
-
-%     hold on
-%     plot3([x(1) xnew(1)],[x(2) xnew(2)],[f(x) f(xnew)],'ro-')
-%     plot3([x(1) xnew(1)],[x(2) xnew(2)],[0 0],'ro-')
-%     refresh
-    niter = niter + 1;
-    % dx = norm(xnew-x);
-    x = xnew;
     
+    xopt = x;
+    fopt = f(xopt);
+    it = it - 1;
 end
-xopt = x;
-fopt = f(xopt);
-niter = niter - 1;
-
