a158c04d7f1e1854b7824a7c9fcb1ce2d10ca7d8
vlaksi <daksrapsody@gmail.com>
2019-12-11 13:15:14 +0100

    Dodana ogranicenja preko max( tj krace uradjeno za PSO)

--- v3/GA-PSO/Rosenbrock function/ogranicenje2_PSO.m
+++ v3/GA-PSO/Rosenbrock function/ogranicenje2_PSO.m
@@ -1,13 +1,13 @@
 function [ f ] = ogranicenje2_PSO(x)
     c=10000;
     % za jednacinu tipa NEJEDNAKOSTI
-    if (-x(2) +1/(x(1)^2) <=0)
-        g1=0;                                   % nema kazne ako je zadovoljeno ogranicenje
-    else
-        g1=c.*(-x(2) +1/(x(1)^2)).^2;           % vrsimo kaznjavanje ako nije
-    end
-    % za jednacinu tipa JEDNAKOSTI
-    g2= c*((-x(2) +x(1).^2 +2).^2);
+    g1=-x(2) +1/(x(1)^2);               
+    p1 = (max(0,g1)).^2;
     
-    f = fun(x) + g1 + g2;
+    % za jednacinu tipa JEDNAKOSTI
+    g2= (-x(2) +x(1).^2 +2);
+    p2=g2.^2;
+    
+    % novi kriterijum optimalnosti
+    f = fun(x) + c.*p1 + c.*p2;
 end
